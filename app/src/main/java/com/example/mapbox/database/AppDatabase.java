0aeafda6762e29868c265bb84187adbeed3aac9a
Oleksandr <oleksandr.bukaliuk.pp.2022@lpnu.ua>
2024-11-11 17:51:56 +0200

    user_profile

--- app/src/main/java/com/example/mapbox/database/AppDatabase.java
+++ app/src/main/java/com/example/mapbox/database/AppDatabase.java
@@ -17,6 +17,12 @@ public class AppDatabase extends SQLiteOpenHelper {
     public static final String COLUMN_LONGITUDE = "longitude";
     public static final String COLUMN_TEXT = "text";
 
+    public static final String TABLE_PROFILE = "user_profile";
+    public static final String COLUMN_PROFILE_ID = "profile_id";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_POINTS = "points";
+    public static final String COLUMN_LEVEL = "level";
+
     // Singleton instance
     private static AppDatabase instance;
 
@@ -42,18 +48,27 @@ public class AppDatabase extends SQLiteOpenHelper {
                 COLUMN_TEXT + " TEXT" +
                 ")";
         db.execSQL(CREATE_TABLE);
-        Log.d("Database", "Table created: " + CREATE_TABLE);
+
+        String CREATE_TABLE_PROFILE = "CREATE TABLE " + TABLE_PROFILE + " (" +
+                COLUMN_PROFILE_ID + " INTEGER PRIMARY KEY AUTOINCREMENT," +
+                COLUMN_NAME + " TEXT," +
+                COLUMN_POINTS + " INTEGER," +
+                COLUMN_LEVEL + " TEXT" +
+                ")";
+        db.execSQL(CREATE_TABLE_PROFILE);
+
+        Log.d("Database", "Tables created: " + CREATE_TABLE + " and " + CREATE_TABLE_PROFILE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PROFILE);
         onCreate(db);
     }
 
     public void insertData(double latitude, double longitude, String text) {
         SQLiteDatabase db = this.getWritableDatabase();
-
         ContentValues values = new ContentValues();
         values.put(COLUMN_LATITUDE, latitude);
         values.put(COLUMN_LONGITUDE, longitude);
@@ -71,4 +86,30 @@ public class AppDatabase extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getReadableDatabase();
         return db.query(TABLE_NAME, new String[]{COLUMN_ID, COLUMN_LATITUDE, COLUMN_LONGITUDE, COLUMN_TEXT}, null, null, null, null, null);
     }
+
+    public void saveUserProfile(String name, int points, String level) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME, name);
+        values.put(COLUMN_POINTS, points);
+        values.put(COLUMN_LEVEL, level);
+
+        int rowsUpdated = db.update(TABLE_PROFILE, values, COLUMN_PROFILE_ID + "= 1", null);
+
+        if (rowsUpdated == 0) {
+            values.put(COLUMN_PROFILE_ID, 1); // ID для першого запису профілю
+            db.insert(TABLE_PROFILE, null, values);
+            Log.d("Database", "User profile inserted.");
+        } else {
+            Log.d("Database", "User profile updated.");
+        }
+        db.close();
+    }
+
+    public Cursor getUserProfile() {
+        SQLiteDatabase db = this.getReadableDatabase();
+        return db.query(TABLE_PROFILE, new String[]{COLUMN_NAME, COLUMN_POINTS, COLUMN_LEVEL}, null, null, null, null, null);
+    }
 }
+
+
